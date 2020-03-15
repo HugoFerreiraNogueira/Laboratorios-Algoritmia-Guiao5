5d3836ac0ada55836046e42a9d6df06d0bbd25f1
Gonçalo Henriques <62214115+Gongas2000@users.noreply.github.com>
2020-03-15 22:35:15 +0000

    Delete interface.h

--- interface.h
+++
@@ -1,7 +0,0 @@
-#include "logica.h"
-#ifndef PROJECTO_INTERFACE_H
-#define PROJECTO_INTERFACE_H
-
-void mostrar_tabuleiro(ESTADO *estado);
-int interpretador(ESTADO *e);
-#endif //PROJECTO_INTERFACE_H
